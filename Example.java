83c7b3ef7a15d99cec3bde0be093a00083d6a6e7
katherinestout <kjs14@hood.edu>
2019-05-23 14:51:17 -0400

    usage

--- Example.java
+++ Example.java
@@ -1,5 +1,9 @@
 public class Example{
     public static void main(String[] args){
-        System.out.println("hello there kat");
+        System.out.println("hello there" + args[0]);
     }
 }
+
+//javac Example.java
+//java Example kat
+//output will be hello there kat
